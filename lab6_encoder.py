6574313f644a18e323d6a035328e7a082ccc5d74
Ben <c.ben.pratt@gmail.com>
2023-10-25 13:16:44 -0400

    Adds Menu and choice settings

--- lab6_encoder.py
+++ lab6_encoder.py
@@ -8,8 +8,21 @@ def encoder(password):
 
 
 def main():
-    password = input()
-    print(encoder(password))
+    while True:
+        print("Menu")
+        print("-"*7)
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        Choice = int(input("do you want to encode, decode or quit?"))
+        if Choice == 1:
+            password = input('What is your password?')
+            print(encoder(password))
+        elif Choice == 2:
+            print("This is where decode goes")
+        elif Choice == 3:
+            break
 
 
 if __name__ == '__main__':
